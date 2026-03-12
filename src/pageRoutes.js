410007d838a5a9f85b6187897cb125403c477e88
Holly Phillips <hollymphillips.dev@gmail.com>
2026-03-11 19:50:06 -0700

    loose page content and page routes file

---
+++ src/pageRoutes.js
@@ -0,0 +1,18 @@
+/*
+    middleware.js must be changed manually
+*/
+
+const pageRoutes = {
+    'index': '/',
+    'account': '/account',
+    'register': '/register',
+    'signin': '/api/auth/signin?callbackUrl=%2F',
+    'signout': '/api/auth/signout?callbackUrl=%2F',
+    'purpose': '/my-core-purpose',
+    'caseStudies': '/case-studies',
+    'portfolio': '/portfolio',
+    'resume': '/resume',
+    'reviews': '/reviews',
+}
+
+export default pageRoutes;
