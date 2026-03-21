5daa27ecee6e35955edf5291f3883735e6bf1033
Holly Phillips <hollymphillips.dev@gmail.com>
2026-03-21 10:43:25 -0700

    remove img

--- src/app/page.js
+++ src/app/page.js
@@ -30,14 +30,6 @@ export default function Home() {
                 </div>
                 <main className="flex min-h-screen items-center justify-center font-sans">
                     <div className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-                        <Image
-                            className=""
-                            src="/next.svg"
-                            alt="Next.js logo"
-                            width={100}
-                            height={20}
-                            priority
-                        />
                         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
                             <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
                                 h1
