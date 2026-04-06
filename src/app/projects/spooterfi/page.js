4866bd57901a526aae9b42783ffa56571db4b7d4
Holly Phillips <hollymphillips.dev@gmail.com>
2026-04-06 01:55:08 -0700

    updated spooterfi video

--- src/app/projects/spooterfi/page.js
+++ src/app/projects/spooterfi/page.js
@@ -7,7 +7,7 @@ export default function Spooterfi() {
             <h1 className="text-center">Spooterfi</h1>
 
             <video width="800" height="338" controls autoPlay={true} muted className="mx-auto my-8">
-                <source src="/projects/gogcomm/gogcomm-site.mp4" type="video/mp4" />
+                <source src="/projects/spooterfi/spooterfi-demo.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
 
