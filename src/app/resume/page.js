ff9ca3605987005326bc4ba51691115eb4bf2185
Holly Phillips <hollymphillips.dev@gmail.com>
2026-04-07 12:56:06 -0700

    moved contact info, images, basically all resume data into the actual resume data json

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -2,29 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const contacts = {
-    'name': 'Holly M. Phillips',
-    'cell': {
-        'href': 'tel+19364251225',
-        'alt': 'phone number: 936-425-1225',
-        'text': '(936) 425-1225'
-    },
-    'email': {
-        'href': 'mailto:hollymphillips.dev@gmail.com?subject=Hello Holly!&body=I\'d love to connect with you...',
-        'alt': 'email address: hollymphillips.dev@gmail.com',
-        'text': 'hollymphillips.dev@gmail.com'
-    },
-    'linkedin': {
-        'href': 'https://www.linkedin.com/in/hooleymcknight/',
-        'alt': 'linkedin profile',
-        'text': 'LinkedIn Profile'
-    },
-    'github': {
-        'href': 'https://www.github.com/hooleymcknight/',
-        'alt': 'github profile',
-        'text': 'GitHub Profile'
-    }
-}
+import { ref, 'Contact Info' as contacts } from '@/app/data/resume.json'; 
 
 export default function Resume() {
 
@@ -32,7 +10,7 @@ export default function Resume() {
         <main className="max-w-[1000px] mx-auto">
             <h1 className="text-center">Resume</h1>
 
-            <Link href="/ruston-coffee.png" download
+            <Link href={ref.downloadHref} download
                 alt="a circular logo of a fictitious coffee shop"
                 className="button mx-auto block w-fit my-3"
             >
@@ -44,14 +22,21 @@ export default function Resume() {
                     <h2>{contacts.name}</h2>
                     {
                         Object.keys(contacts).filter(x => typeof(contacts[x]) === 'object').map(x => 
-                            <Link className="hover:underline" key={contacts[x].text} href={contacts[x].href} alt={contacts[x].alt}>{contacts[x].text}</Link>
+                            <Link key={contacts[x].text}
+                                className="hover:underline"
+                                href={contacts[x].href}
+                                alt={contacts[x].alt}
+                                target="_blank"
+                            >
+                                {contacts[x].text}
+                            </Link>
                         )
                     }
                 </div>
 
                 <div className="resume-img block mx-auto w-fit max-w-[200px]">
-                    <Image className="img" src="/brick-grey.png" width={320} height={320} alt="Holly standing in front of a brick wall with an open smile" />
-                    <Image className="img-hover" src="/brick-color.jpg" width={320} height={320} alt="Holly standing in front of a brick wall with an open smile" />
+                    <Image priority className="img" src={ref.selfImage} width={320} height={320} alt={ref.selfImageAlt} />
+                    <Image priority className="img-hover" src={ref.selfImageHover} width={320} height={320} alt={ref.selfImageHoverAlt} />
                 </div>
             </div>
 
