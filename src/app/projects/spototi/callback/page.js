c3dc0310993b9a981e454f1737cefd5b639e044c
Holly M. Phillips <hollymphillips.dev@gmail.com>
2026-06-01 19:23:51 -0700

    oh my god its async

--- src/app/projects/spototi/callback/page.js
+++ src/app/projects/spototi/callback/page.js
@@ -1,7 +1,7 @@
 'use server'
 import { NextRequest, NextResponse } from 'next/server';
 
-export function GET(request) {
+export async function GET(request) {
     const search = request.nextUrl.search;
     return new NextResponse(null, {
         status: 302,
