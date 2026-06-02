a0ec67128e4e972ef50de5565f562cbd1d473c70
Holly M. Phillips <hollymphillips.dev@gmail.com>
2026-06-01 19:18:54 -0700

    wrong kind of callback setup, fixed

--- src/app/projects/spototi/callback/page.js
+++ src/app/projects/spototi/callback/page.js
@@ -1,12 +1,9 @@
-// import { GetServerSideProps } from "next";
+import { NextRequest, NextResponse } from 'next/server';
 
-export const getServerSideProps = async ({ query, res }) => {
-    const qs = new URLSearchParams(query).toString();
-    res.writeHead(302, { Location: `spototi://callback${qs ? '?' + qs : ''}`});
-    res.end();
-    return { props: {} };
-}
-
-export default function SpototiCallback() {
-    return null;
+export function GET(request) {
+    const search = request.nextUrl.search;
+    return new NextResponse(null, {
+        status: 302,
+        headers: { Location: `spototi://callback${qs ? '?' + qs : ''}` },
+    });
 }
